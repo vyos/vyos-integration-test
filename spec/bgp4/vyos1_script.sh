037fd6a56255854e87333223f8966a1dfa339fc3
root <root@vyos-dev01>
2017-09-22 18:25:21 +0900

    bgp4: added iBGP peer

--- spec/bgp4/vyos1_script.sh
+++ spec/bgp4/vyos1_script.sh
@@ -6,12 +6,16 @@ fi
 
 source /opt/vyatta/etc/functions/script-template
 
-set protocols bgp 65536 neighbor 10.0.1.12 ebgp-multihop 2 
-set protocols bgp 65536 neighbor 10.0.1.12 remote-as 65537 
+set protocols bgp 65536 parameters router-id 10.0.1.11
+
+set protocols bgp 65536 neighbor 10.0.1.12 remote-as 65537
 set protocols bgp 65536 neighbor 10.0.1.12 update-source 10.0.1.11
 set protocols bgp 65536 neighbor 10.0.1.12 advertisement-interval 1
-set protocols bgp 65536 network 10.0.2.0/24
-set protocols bgp 65536 parameters router-id 10.0.1.11
+
+set protocols bgp 65536 neighbor 10.0.2.13 remote-as 65536
+set protocols bgp 65536 neighbor 10.0.2.13 update-source 10.0.2.11
+set protocols bgp 65536 neighbor 10.0.2.13 advertisement-interval 1
+set protocols bgp 65536 neighbor 10.0.2.13 nexthop-self
 
 commit
 save
--- spec/bgp4/vyos2_script.sh
+++ spec/bgp4/vyos2_script.sh
@@ -6,12 +6,16 @@ fi
 
 source /opt/vyatta/etc/functions/script-template
 
-set protocols bgp 65537 neighbor 10.0.1.11 ebgp-multihop 2 
-set protocols bgp 65537 neighbor 10.0.1.11 remote-as 65536 
+set protocols bgp 65537 parameters router-id 10.0.1.12
+
+set protocols bgp 65537 neighbor 10.0.1.11 remote-as 65536
 set protocols bgp 65537 neighbor 10.0.1.11 update-source 10.0.1.12
 set protocols bgp 65537 neighbor 10.0.1.11 advertisement-interval 1
-set protocols bgp 65537 network 10.0.3.0/24
-set protocols bgp 65537 parameters router-id 10.0.1.12
+
+set protocols bgp 65537 neighbor 10.0.3.14 remote-as 65537
+set protocols bgp 65537 neighbor 10.0.3.14 update-source 10.0.3.12
+set protocols bgp 65537 neighbor 10.0.3.14 advertisement-interval 1
+set protocols bgp 65537 neighbor 10.0.3.14 nexthop-self
 
 commit
 save
